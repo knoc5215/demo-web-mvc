7fe9e224d958ff71258aa839101ec3bfb94a9901
knoc5215 <knoc5215@naver.com>
2020-12-28 22:49:42 +0900

    핸들러 메소드 16부 @ResponseBody & ResponseEntity

--- src/main/java/me/jumen/demowebmvc/EventApi.java
+++ src/main/java/me/jumen/demowebmvc/EventApi.java
@@ -1,27 +1,40 @@
 package me.jumen.demowebmvc;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.validation.Valid;
+
+@Controller
 @RequestMapping("/api/events")
 public class EventApi {
 
     @PostMapping()
-    public Event createEvent(HttpEntity<Event> request) {
+    @ResponseBody
+    public Event createEvent(/*HttpEntity<Event> request,*/ @RequestBody Event event, BindingResult bindingResult) {
         // save
-        MediaType contentType = request.getHeaders().getContentType();  //@RequestBody와 비슷하지만, 추가적으로 헤더 정보를 가져올 수 있다
-        System.out.println(contentType);
+//        MediaType contentType = request.getHeaders().getContentType();  //@RequestBody와 비슷하지만, 추가적으로 헤더 정보를 가져올 수 있다
+//        System.out.println(contentType);
 
-//        if (bindingResult.hasErrors()) {
-//            bindingResult.getAllErrors().forEach(System.out::println);
-//        }
-        return request.getBody();
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(System.out::println);
+        }
+//        return request.getBody();
+        return event;
+    }
+
+    @PostMapping("/responseEntity")
+    public ResponseEntity createEventResponseEntity(@RequestBody @Valid Event event, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return new ResponseEntity<Event>(event, HttpStatus.CREATED);
     }
 
 }
--- src/test/java/me/jumen/demowebmvc/EventApiTest.java
+++ src/test/java/me/jumen/demowebmvc/EventApiTest.java
@@ -32,11 +32,29 @@ class EventApiTest {
 
         mockMvc.perform(post("/api/events")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(json))
+                    .content(json)
+                    .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("name").value("jumen"))
                 .andExpect(jsonPath("limit").value(5215))
+        ;
+    }
+
+    @Test
+    public void createEventResponseEntity() throws Exception {
+        Event event = new Event();
+
+        event.setName("jumen");
+        event.setLimit(-5215);
+
+        String json = objectMapper.writeValueAsString(event);
+
+        mockMvc.perform(post("/api/events/responseEntity")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
         ;
     }
 
