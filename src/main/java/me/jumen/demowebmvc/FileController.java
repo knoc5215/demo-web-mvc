0396e85e8a8f4d724441d69a69bdf7b5bde346c3
knoc5215 <knoc5215@naver.com>
2020-12-28 22:22:09 +0900

    핸들러 메소드 14부 ResponseEntity

--- src/main/java/me/jumen/demowebmvc/FileController.java
+++ src/main/java/me/jumen/demowebmvc/FileController.java
@@ -1,12 +1,19 @@
 package me.jumen.demowebmvc;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class FileController {
@@ -23,5 +30,24 @@ public class FileController {
         redirectAttributes.addFlashAttribute("message", message);   // model에 자동으로 담긴다
         return "redirect:/file";
 
+    }
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @GetMapping("/file/{filename}")
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) throws
+            IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+        Tika tika = new Tika();
+        String type = tika.detect(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" +
+                        resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, type)
+                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
+                .body(resource);
     }
 }
